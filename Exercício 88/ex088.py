ef6d60f20a766326b6eac98f65494a537e45c4b2
AdemirRamos <ademirramos634@gmail.com>
2021-11-29 00:30:15 -0300

    Atualização do Exercício 88

--- Exercício 88/ex088.py
+++ Exercício 88/ex088.py
@@ -1,6 +1,6 @@
 print('Palpites Para a Mega-Sena')
 print('-=' * 30)
-print('{:^20} Jogo da Mega-Sena')
+print('Jogo da Mega-Sena')
 import random
 jogo_mega = []
 jogo = int(input('Quantos jogos você quer que eu sorteie? Digite um número: '))
@@ -15,4 +15,32 @@ for c in range(1, jogo + 1):
 
 print('Palpites Para a Mega-Sena')
 print('-=' * 30)
-print('{:^20} Jogo da Mega-Sena')
+print('Jogo da Mega-Sena')
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+print('-' * 50)
+print('     Números Para Jogar Na Mega-Sena.     ')
+print('-' * 50)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'Sorteando {quant} jogos.', '-=' * 3)
+print('Os números sorteado foram: {jogos}.')
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}.')
+    sleep(1)
+print('-=' * 5, 'Boa Sorte!', '-=' * 5)
