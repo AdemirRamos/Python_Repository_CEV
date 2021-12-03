48fff15fe15fcf033f6534fbcf94cb3b17714792
AdemirRamos <ademirramos634@gmail.com>
2021-12-02 22:17:33 -0300

    Adição do Exercício 99

---
+++ Curso Em Vídeo/Exercício 99/ex099.py
@@ -0,0 +1,16 @@
+import math
+print('Exercício 99')
+print()
+lista = list()
+
+def valores():
+    contador = math.randint(0, 10)
+    print('Analisando os dados informados...')
+    for c in range(0, contador):
+        lista.clear
+        lista.append(contador)
+        print(f'{contador}', end='')
+        print('Ao todo, foram informados')
+
+valores()
+print(lista)
