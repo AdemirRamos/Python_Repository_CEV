435f6e3dfe73fd1ff44069ca5b496d2aaaafcec1
AdemirRamos <ademirramos634@gmail.com>
2021-12-11 12:31:05 -0300

    Atualização do Exercício 99

--- Curso Em Vídeo/Exercício 99/ex099.py
+++ Curso Em Vídeo/Exercício 99/ex099.py
@@ -1,6 +1,6 @@
-import random
-print('Exercício 99')
+print('Função Que Descobre o Maior Número')
 print()
+import random
 lista = list()
 lista_2 = list()
 
@@ -16,3 +16,31 @@ def valores():
 valores()
 print(lista)
 print(f'O maior valor, entre os números na lista, foi: {max(lista)}.')
+
+#Resolução do Guanabara:
+
+print('Função Que Descobre o Maior Número')
+print()
+
+from time import sleep
+
+def maior(* valores):
+    cont = maior = 0
+    print('-=' * 30)
+    print('Analisando os valores passados...')
+    for valor in valores:
+        print(f'{valor}', end=' ')
+        sleep(0.3)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont += 1
+    print(f'Foram informados {valores} e o maior valor foi: {maior}.')
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior(0)
