20cafa6a8500a50c2edfc463b4e45271ba7df7d2
AdemirRamos <ademirramos634@gmail.com>
2021-12-11 10:39:56 -0300

    Atualização do Exercício 98

--- Curso Em Vídeo/Exercício 98/ex098.py
+++ Curso Em Vídeo/Exercício 98/ex098.py
@@ -1,4 +1,4 @@
-print('Exercício 98')
+print('Função de Contador')
 print()
 
 def um(valor):
@@ -32,3 +32,47 @@ begin = int(input('Digite o ínicio da contagem: '))
 end = int(input('Digite o fim da contagem: '))
 step = int(input('Digite o passo da contagem: '))
 your_count(step)
+
+#Resolução do Guanabara:
+
+print('Função de Contador')
+print()
+
+from time import sleep
+
+def contador(i, f, p):
+
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    
+    print('-=' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}.')
+    sleep(2.5)
+        
+    if i < f:
+        contador = i
+        while contador <= f:
+            print(f'{contador}', end=' ')
+            sleep(0.5)
+            contador += p
+        print('Fim!')
+    else:
+        contador = i
+        while contador >= f:
+            print(f'{contador}', end=' ')
+            #A depender da versão do PyCharm, pode ocorrer um erro ao se usar o "sleep".
+            #Para se evitar esse erro (caso ele aconteça), basta adicionar, após "end=''", o seguinte comando: "flush = True".
+            sleep(0.5)
+            contador -= p
+        print('Fim!')
+    
+contador(0, 100, 10)
+contador(10, 0, 2)
+print('-=' * 20)
+print('Agora e a sua vez de personalizar a contagem!')
+início = int(input('Ínicio: '))
+fim = int(input('Fim:       '))
+passo = int(input('Passo:   '))
+contador(início, fim, passo)
