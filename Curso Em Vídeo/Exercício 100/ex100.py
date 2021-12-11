12d4b5ce25afaec07a1f13c3593ce3f1756e057b
AdemirRamos <ademirramos634@gmail.com>
2021-12-11 12:47:18 -0300

    Atualização do Exercício 100

--- Curso Em Vídeo/Exercício 100/ex100.py
+++ Curso Em Vídeo/Exercício 100/ex100.py
@@ -1,4 +1,4 @@
-print('Exercício 100')
+print('Função Para Sortear e Somar Números (Pares)')
 print()
 import random
 lista = list()
@@ -11,3 +11,31 @@ for c in range(contador, quantidade):
     lista.append(quantidade)
     lista.clear
 print(lista, end=' ')
+
+#Resolução do Guanabara:
+
+print('Função Para Sortear e Somar Números (Pares)')
+print()
+
+from random import randint
+from time import sleep
+
+def sorteio(lista):
+    print('Sorteando 5 valores da lista...', end=' ')
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n}', end=' ', flush = True)
+        sleep(0.3)
+    print('Pronto!')
+
+def soma_par(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares da lista "{lista}", temos a seguinte soma: {soma}.')
+
+números = list()
+sorteio(números)
+soma_par(números)
