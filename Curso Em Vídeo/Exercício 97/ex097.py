31b041afc396db428506e88e0e94b601ab155d18
AdemirRamos <ademirramos634@gmail.com>
2021-12-11 10:37:41 -0300

    Atualização do Exercício 97

--- Curso Em Vídeo/Exercício 97/ex097.py
+++ Curso Em Vídeo/Exercício 97/ex097.py
@@ -23,3 +23,18 @@ linha_2(string_2)
 print()
 string_3 = str(print('Exercício 97'))
 linha_3(string_3)
+
+#Resolução do Guanabara:
+
+print('Um Print Especial')
+print()
+
+def escreva(message):
+    tamanho = len(message) + 4
+    print('~' * tamanho )
+    print(f'  {message}')
+    print('~' * tamanho)
+    
+escreva('Gustavo Guanabara.')
+escreva('Curso de Python no YouTube.')
+escreva('CEV.')
