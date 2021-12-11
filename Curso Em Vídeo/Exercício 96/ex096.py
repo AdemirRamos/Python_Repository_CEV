69f0075ceb2c075330897ae9ec3ea71b16e52c08
AdemirRamos <ademirramos634@gmail.com>
2021-12-11 10:36:27 -0300

    Atualização do Exercício 96

--- Curso Em Vídeo/Exercício 96/ex096.py
+++ Curso Em Vídeo/Exercício 96/ex096.py
@@ -7,3 +7,18 @@ def cálculo(altura, largura):
 altura = float(input('Digite a altura: '))
 largura = float(input('Digite a largura: '))
 cálculo(altura, largura)
+
+#Resolução do Guanabara:
+
+print('Função Que Calcula Área')
+print()
+
+def área(largura, comprimento):
+    a = largura * comprimento
+    print(f'A área de um terreno {largura} x {comprimento} é igual a {a}m².')
+
+print('Controle de terrenos.')
+print('-' * 20)
+l = float(input('Largura (m): '))
+c = float(input('Comprimento (m): '))
+área(l, c)
