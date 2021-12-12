e89f00e2daf362f67c9cc7808a77a90a751ae3d3
AdemirRamos <ademirramos634@gmail.com>
2021-12-11 21:05:54 -0300

    Atualização do Exercício 101

--- Curso Em Vídeo/Exercício 101/ex101.py
+++ Curso Em Vídeo/Exercício 101/ex101.py
@@ -12,3 +12,30 @@ def voto():
     else:
         print(f'Você tem {idade} anos de idade. A sua idade é maior do que 65 anos. Para você, o voto é facultativo.')
 voto()
+
+#Resolução do Guanabara:
+
+print('Função Para Votações')
+print()
+
+def voto(ano):
+
+    #Escopo de importação: se configura quando uma importação é feita somente para um trecho específico do programa. 
+
+    from datetime import date
+    
+    atual = date.today().year
+    idade = atual - ano
+
+    if idade <= 16:
+        return f'Com {idade} anos, o voto não é permitido.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos, o voto é opcional.'
+    else:
+        return f'Com {idade} anos, o voto é obrigatório.'
+
+#Linhas opcionais:
+
+#nascimento = int(input('Em que ano você nasceu? '))
+print(voto(1990))
+#print(voto(nascimento))
