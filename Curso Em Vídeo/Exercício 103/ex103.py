7ad167a73aa95baed7e8f669ea2285c8d5d1593e
AdemirRamos <ademirramos634@gmail.com>
2021-12-11 21:09:23 -0300

    Atualização do Exercício 103

--- Curso Em Vídeo/Exercício 103/ex103.py
+++ Curso Em Vídeo/Exercício 103/ex103.py
@@ -1,4 +1,4 @@
-print('Exercício 103')
+print('Ficha do Jogador')
 print()
 def jogador(nome, gols):
     nome = 'desconhecido'
@@ -7,3 +7,25 @@ def jogador(nome, gols):
 nome = str(input('Digite o nome do jogador: '))
 gols = str(input('Digite o número de gols marcados pelo jogador: '))
 jogador(nome, gols)
+
+#Resolução do Guanabara:
+
+print('Ficha do Jogador')
+print()
+
+def ficha(jogador = '"desconhecido"', gols = 0):
+    print(f'O jogador {jogador} fez {gols} gol (s) no campeonato.')
+    
+    
+n = str(input('Nome do jogador: '))
+g = str(input('Número de gols: '))
+
+if g.isnumeric():
+    g = int(g) #Transforma o "g str" em um "g int".
+else:
+    g = 0
+
+if n.strip() == '':
+    ficha(gols = g)
+else:
+    ficha(n, g)
