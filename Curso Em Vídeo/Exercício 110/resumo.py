4221f7c57e8ab961a8d6a30976f97aa1d0252298
AdemirRamos <ademirramos634@gmail.com>
2021-12-17 09:54:44 -0300

    Atualização do Arquivo "resumo.py" (Ex110)

--- Curso Em Vídeo/Exercício 110/resumo.py
+++ Curso Em Vídeo/Exercício 110/resumo.py
@@ -7,19 +7,19 @@ def string():
     print('~' * 35)
 
 def math(a, b, c):
-    print(f'Preço analisado: {a:>14}')
+    print(f'Preço analisado: R${a:>14}')
     
     dobro = a * 2
-    print(f'Dobro do valor analisado: {dobro:>5}')
+    print(f'Dobro do valor analisado: R${dobro:>5}')
     
     metade = a / 2
-    print(f'Metade do valor analisado: {metade:>5}')
+    print(f'Metade do valor analisado: R${metade:>5}')
 
     vinte = (a * 20 / 100)
     increase = vinte + a
-    print(f'20% de aumento: {increase:>16}')
+    print(f'20% de aumento: R${increase:>16}')
 
     doze = (a * 12 / 100)
     redução = a - doze
-    print(f'12% de redução: {redução:>16.1f}')
+    print(f'12% de redução: R${redução:>16.1f}')
     print('~' * 35)
