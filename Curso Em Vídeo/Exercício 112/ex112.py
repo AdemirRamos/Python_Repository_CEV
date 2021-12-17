c5ad30ca669c8765c3f5318aa6c9535ebd918bc7
AdemirRamos <ademirramos634@gmail.com>
2021-12-17 09:56:09 -0300

    Adição do Exercício 112

---
+++ Curso Em Vídeo/Exercício 112/ex112.py
@@ -0,0 +1,17 @@
+print('Entrada de Dados Monetários')
+print()
+
+import resumo
+
+def leia_dinheiro(message):
+    válido = False
+    while not válido:
+        entrada = str(input(message)).replace(',', '.').strip()
+        if entrada.isalpha() or entrada == '':
+            print(f'\033[0;31mErro: \"{entrada}\" é um preço inválido.\033[m')
+        else:
+            válido = True
+            return float(entrada)
+
+preço = leia_dinheiro('Digite o preço: R$')
+resumo.math(preço, 35, 22)
