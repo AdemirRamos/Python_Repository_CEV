21514d1585f028647dd3b44cbc539b2dd8b7c397
AdemirRamos <ademirramos634@gmail.com>
2021-12-22 10:41:35 -0300

    Adição do Exercício 13

---
+++ Curso Em Vídeo/Exercício 13/ex013.py
@@ -0,0 +1,7 @@
+
+print('Reajuste Salarial')
+print()
+
+salário = float(input('Qual é o salário do funcionário: R$'))
+increase = salário + (salário * 15 / 100)
+print(f'\nO salário do funcionário, após um reajuste de 15%, será igual a: R${increase:.2f}.')
