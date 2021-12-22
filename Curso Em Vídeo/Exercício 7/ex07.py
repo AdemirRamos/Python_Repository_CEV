cacdb53af8dbf30a3f43a59e8a8b9c74db1c08c8
AdemirRamos <ademirramos634@gmail.com>
2021-12-22 10:34:48 -0300

    Adição do Exercício 7

---
+++ Curso Em Vídeo/Exercício 7/ex07.py
@@ -0,0 +1,6 @@
+print('Média Aritmética')
+print()
+
+n1 = float(input('Digite a 1ª nota do (a) aluno (a): '))
+n2 = float(input('Digite a 2ª nota do (a) aluno (a): '))
+print(f'\nA média entre a 1ª nota ({n1}) e a 2ª nota ({n2}) é igual a: {(n1 + n2) / 2:.1f}.')
