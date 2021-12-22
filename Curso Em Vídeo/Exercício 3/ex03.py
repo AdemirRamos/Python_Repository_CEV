cb77b3b2070c050b5968039a793a71608c29fa02
AdemirRamos <ademirramos634@gmail.com>
2021-12-22 10:25:58 -0300

    Adição do Exercício 3

---
+++ Curso Em Vídeo/Exercício 3/ex03.py
@@ -0,0 +1,6 @@
+print('Somando Dois Números')
+print()
+
+número_1 = int(input('Digite um valor: '))
+número_2 = int(input('Digite outro valor: '))
+print(f'\nA soma entre {número_1} + {número_2} é igual a: {número_1 + número_2}.')
